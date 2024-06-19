eb662158be8ae1367296da78cc5221447a85c787
micoyan <micoyan52@gmail.com>
2024-06-19 03:40:03 +0300

    fist lesson changes commit

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,5 +1,14 @@
+pluginManagement {
+    val kotlinVersion: String by settings
+    plugins {
+        kotlin("jvm") version kotlinVersion
+    }
+}
+
 plugins {
     id("org.gradle.toolchains.foojay-resolver-convention") version "0.5.0"
 }
+
 rootProject.name = "marketplace"
 
+include("m1l1-first")
